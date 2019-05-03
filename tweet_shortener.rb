662c225d6273b6d1fc79833a50f5176ca73097de
Keith Burgie <krburgie@gmail.com>
2019-05-03 02:08:29 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,4 @@
-tweet = ["I'm running out of example tweets for you guys, which is weird, because I'm a writer and this is just writing and I tweet all day. For real, you guys. For real."]
+tweet = "I'm running out of example tweets for you guys, which is weird, because I'm a writer and this is just writing and I tweet all day. For real, you guys. For real."
 
 tweets = [
   "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!", "OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?", "I'm running out of example tweets for you guys, which is weird, because I'm a writer and this is just writing and I tweet all day. For real, you guys. For real.", "GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!", "New game. Middle aged tweet followed by #youngPeopleHashTag Example: Gotta get my colonoscopy and mammogram soon. Prevention is key! #swag"
@@ -11,6 +11,7 @@ def dictionary
     "two" => "2", 
     "too" => "2", 
     "for" => "4",
+    "For" => "4",
     "four" => "4",
     "be" => "b",
     "you" => "u",
@@ -22,12 +23,11 @@ end
 def word_substituter(tweet)
     array = tweet.split(" ")
     array.collect! do |word|
-      puts word.downcase
-      #dictionary.keys.include?(word.downcase) ? word = dictionary[word] : word
+      dictionary.keys.include?(word) ? word = dictionary[word] : word
     end
     array.join(" ")
 end
-word_substituter(tweet)
+puts word_substituter(tweet)
 
 def bulk_tweet_shortener(tweets)
   tweets.each do |tweet|
